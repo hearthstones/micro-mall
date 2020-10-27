5d19743b05214e153385cda176c05057690c9ef5
luchao <luchao@sunlands.com>
2020-10-27 17:28:17 +0800

    feat:init mall-monitor.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # micro-mall
 仿照mall-swarm搭建一个微服务架构商城
+
+## 项目结构
+- mall-gateway 微服务网关（Spring Cloud Gateway）
+- mall-monitor 微服务监控（Spring Boot Admin）
---
+++ mall-monitor/src/test/java/com/micro/mall/monitor/MallMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.mall.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
